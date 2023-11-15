0debc1f9d352044e890b729487a016069ec6f98a
Paweł Marks <pmarks@virtuslab.com>
2023-11-15 16:37:35 +0100

    Move SSH factory and add brnaching point to new branch

--- Repo.scala
+++ Repo.scala
@@ -6,6 +6,10 @@ import scala.jdk.CollectionConverters.*
 import os.Path
 import org.eclipse.jgit.api.Git
 import org.eclipse.jgit.revwalk.RevCommit
+import org.eclipse.jgit.transport.sshd.SshdSessionFactory
+import org.eclipse.jgit.transport.SshSessionFactory
+import org.eclipse.jgit.transport.sshd.JGitKeyCache
+import org.eclipse.jgit.transport.sshd.DefaultProxyDataFactory
 
 class Repo private (private[ogrodnik] val git: Git):
   def resolve(rev: String): Commit =
@@ -50,12 +54,16 @@ class Repo private (private[ogrodnik] val git: Git):
     git.checkout().setName(branch).call()
     block(using this, new OnBranch { val name = branch })
 
-  def onNewBranch[T](branch: String)(block: (Repo, OnBranch) ?=> T): T =
-    git.checkout().setCreateBranch(true).setName(branch).call()
-    block(using this, new OnBranch { val name = branch })
+  def onNewBranch[T](branchPoint: String, name: String)(block: (Repo, OnBranch) ?=> T): T =
+    git.checkout().setName(branchPoint).call()
+    git.pull().call()
+    git.checkout().setCreateBranch(true).setName(name).call()
+    block(using this, new OnBranch { val name = name })
 
 object Repo:
   def open(path: Path): Repo =
+    val factory = new SshdSessionFactory(new JGitKeyCache, new DefaultProxyDataFactory)
+    SshSessionFactory.setInstance(factory)
     new Repo(Git.open(path.toIO))
 
 def git(using repo: Repo): Git = repo.git
